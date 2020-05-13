ee873846b196d2d797607ffd2083115c847d897e
m-haketa <git@excelspeedup.com>
2020-05-14 07:41:56 +0900

    firebaseログイン、userID取得

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,9 +1,36 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import * as firebase from 'firebase';
 
 const rootCollection = 'restaurants';
 
+export const firebaseConfig = {
+  projectId: 'tutorial-277014',
+  databaseURL: 'https://tutorial-277014.firebaseio.com',
+  storageBucket: 'tutorial-277014.appspot.com',
+  locationId: 'asia-northeast1',
+  apiKey: 'AIzaSyDgUtOthKXWACyhm8BXM65NI40NqUgdeoc',
+  authDomain: 'tutorial-277014.firebaseapp.com',
+  messagingSenderId: '1058197441056',
+};
+
 export const App: React.FC = () => {
+  const [user, setUser] = useState<firebase.auth.UserCredential>();
+
+  useEffect(() => {
+    console.log('setuser async');
+    firebase.initializeApp(firebaseConfig);
+
+    firebase
+      .auth()
+      .signInAnonymously()
+      .then((u) => {
+        setUser(u);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }, []);
+
   /*
   const [data, setData] = useState({} as any);
 
@@ -29,5 +56,5 @@ export const App: React.FC = () => {
 
   return <div>{data}</div>;
   */
-  return <div>App</div>;
+  return <div>App: {user && user.user && user.user.uid}</div>;
 };
