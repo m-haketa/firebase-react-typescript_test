535e89c0139bd58cdd62513421d90f767b5dd329
m-haketa <git@excelspeedup.com>
2020-05-14 07:08:06 +0900

    tsxファイルのうち不要部分を削除

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,5 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import * as firebase from 'firebase';
+
+const rootCollection = 'restaurants';
 
 export const App: React.FC = () => {
+  /*
+  const [data, setData] = useState({} as any);
+
+  const app = firebase.app();
+
+  const query = firebase
+    .firestore()
+    .collection(rootCollection)
+    .orderBy('avgRating', 'desc')
+    .limit(50);
+
+  query.onSnapshot((snapshot) => {
+    if (!snapshot.size) return null;
+
+    snapshot.docChanges().forEach((change) => {
+      if (change.type === 'removed') {
+        console.log('removed');
+      } else {
+        setData(change.doc);
+      }
+    });
+  });
+
+  return <div>{data}</div>;
+  */
   return <div>App</div>;
 };
