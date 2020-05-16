a7b9db19f3416078c159ce041d8d4140ba04c0f0
m-haketa <git@excelspeedup.com>
2020-05-16 14:42:21 +0900

    timestampだけでなくDateもプリミティブとして扱う

--- src/client/firestoreWrapper/type.ts
+++ src/client/firestoreWrapper/type.ts
@@ -1,15 +1,32 @@
 export type Timestamp = firebase.firestore.Timestamp;
 
+//collection-document-collection・・・の構造を、オブジェクトで表す。
+//{
+//  collection1: {
+//    documentprops1: string;
+//    ...
+//    documentpropsn: boolean;
+//
+//    collection2: {
+//      documentprops1: string;
+//      ...
+//      documentpropsn: boolean;
+//    }
+//  }
+//}
+//中身がobject（Date、Timestampを除く）のものはcollectionとみなす。
+//残りは、documentとして取り扱う。
+//
+//そこで、propertyのデータ型がprimitiveかどうかを判断するtypeを作る。
+//TimestampとDateはPrimitiveとして扱う
 export type KeyOfPrimitiveValue<T> = {
-  [K in keyof T]: T[K] extends string
+  [K in keyof T]: T[K] extends Timestamp
     ? K
-    : T[K] extends number
+    : T[K] extends Date
     ? K
-    : T[K] extends boolean
-    ? K
-    : T[K] extends Timestamp
-    ? K
-    : never;
+    : T[K] extends object
+    ? never //Timestamp、Date型以外のobjectは、collectionの名前とみなす
+    : K;
 } extends { [_ in keyof T]: infer X }
   ? X
   : never;
