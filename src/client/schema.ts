6decd3da3c4fb5b47f1e31ac45e7f616db9a39f3
m-haketa <git@excelspeedup.com>
2020-05-17 13:30:45 +0900

    不要部分削除

--- src/client/schema.ts
+++ src/client/schema.ts
@@ -1,9 +1,4 @@
-import type {
-  Timestamp,
-  DocumentProps,
-  Substitute,
-} from './firestoreWrapper/type';
-import { firestore as f } from './firestoreWrapper/Firestore';
+import type { Timestamp } from './firestoreWrapper/type';
 
 //propsがオブジェクトの場合はcollectionを表す
 export interface Database {
@@ -24,59 +19,3 @@ export interface Database {
     };
   };
 }
-
-export interface ConvertedData {
-  restaurants: {
-    avgRating: number;
-    category: string;
-    city: string;
-    name: string;
-    numRatings: number;
-    photo: string;
-    price: number;
-    ratings: {
-      rating: number;
-      text: string;
-      timestamp: Date;
-      userId: string;
-      userName: string;
-    };
-  };
-}
-
-type Test = DocumentProps<Database['restaurants']>;
-
-const firestore = f<Database>();
-
-const col = firestore.collection('restaurants');
-
-const nextCol = col.doc('aaa').collection('ratings');
-
-async () => {
-  const data = await nextCol.get();
-  const docs = data.docs;
-  docs[0].data().timestamp;
-};
-
-async () => {
-  const data = await col.get();
-  const docs = data.docs;
-  docs[0].data().avgRating;
-};
-
-async () => {
-  const document = col.doc('abcde');
-  const data = await document.get();
-  const docs = data.data();
-  docs?.category;
-};
-
-export interface R {
-  rating: number;
-  text: string;
-  timestamp: string;
-  userId: string;
-  userName: string;
-}
-
-type Rs = Substitute<R, { timestamp: Date }>;
